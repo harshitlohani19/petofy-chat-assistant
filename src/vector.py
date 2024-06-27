f87bfb59213e8192a4de7f91fcd95f97067414c8
harshitlohani19 <harshitlohani2002@gmail.com>
2024-06-27 12:55:01 +0530

    Prompt fixes

--- src/vector.py
+++ src/vector.py
@@ -10,7 +10,14 @@ combined_data = load_json()
 def generate_embeddings(combined_data):
     for c in combined_data:
         abcd = client.embeddings.create(input=c, model="text-embedding")
+        embeddings_data = abcd["data"][0]["embedding"]
+        print(embeddings_data)
         return abcd
 
 
-generate_embeddings(combined_data)
+#     # Save embeddings to a JSON file
+#     with open(output_file, "w") as file:
+#         json.dump(embeddings, file, indent=4)
+
+
+# generate_embeddings(combined_data)
