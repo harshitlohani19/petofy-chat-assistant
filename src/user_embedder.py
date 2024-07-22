0b427263c465093a9c78a8b6bb54d793cbf81083
Lohaniharshit <harshit.lohani@cynoteck.com>
2024-07-22 18:06:14 +0530

    changes

--- src/user_embedder.py
+++ src/user_embedder.py
@@ -12,6 +12,6 @@ class User_embedding(Embedder):
     def custom_embedder(self):
         pass
 
-    def set_embedder(self,embedder_callback,):
+    def set_embedder(self, embedder_callback,):
         result = embedder_callback()
         return result
