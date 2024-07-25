6c122b8809cdb5f8d97dad98a1e855437e027ae9
Lohaniharshit <harshit.lohani@cynoteck.com>
2024-07-25 21:34:02 +0530

    minor change

--- src/user_vector_config.py
+++ src/user_vector_config.py
@@ -4,6 +4,3 @@ class VectorConfig(BaseVector):
     def __init__(self):
         super().__init__()
 
-    def set_vector(self, vector_callback, *args):
-        result = vector_callback(*args)
-        return result
